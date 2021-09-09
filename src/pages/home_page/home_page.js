812f212b0ac80dedd78bded1c12cd92d0a813f6b
Nimeshika Kumuduni <nimeshika.20@arimac.xyz>
2021-09-09 18:56:38 +0530

    k

--- src/pages/home_page/home_page.js
+++ src/pages/home_page/home_page.js
@@ -1,22 +1,12 @@
-import { Card, Grid } from "@material-ui/core";
-import { useStoreState } from "easy-peasy";
 import React, { useEffect } from "react";
 import {
   getItemCategoriesCall,
   getRestaurantCall,
 } from "../../api_service/api_service";
-import AppBar from "../../components/app_bar/app_bar";
-import Item from "../../components/item/item";
-import ItemCategory from "../../components/item_category/item_category";
 import useStyles from "./home_page_styles";
 
 export default function HomePage() {
   const classes = useStyles();
-  const itemCategories = useStoreState((state) => state.itemCategories);
-
-  const selectedItemCategory = useStoreState(
-    (state) => state.selectedItemCategory
-  );
 
   useEffect(() => {
     getRestaurantCall();
@@ -25,34 +15,7 @@ export default function HomePage() {
 
   return (
     <div className={classes.root}>
-      <div className={classes.left_bar}>
-        <Card className={classes.logo_outer}></Card>
-        {itemCategories.map((category, index) => (
-          <ItemCategory
-            key={index}
-            category={category}
-            isSelected={
-              selectedItemCategory
-                ? selectedItemCategory.id === category.id
-                : false
-            }
-          />
-        ))}
-      </div>
-      <div className={classes.right_bar}>
-        <AppBar />
-        <div className={classes.main_row}>
-          <div className={classes.main_left_container}>
-            <Grid style={{ width: "100%" }} container spacing={3}>
-              {selectedItemCategory &&
-                selectedItemCategory.items.map((item, index) => (
-                  <Item adminView={false} item={item} key={index} index={index} />
-                ))}
-            </Grid>
-          </div>
-          <div className={classes.main_right_container}></div>
-        </div>
-      </div>
+     
     </div>
   );
 }
---
+++ src/pages/landing_page/landing_page.js
@@ -0,0 +1,11 @@
+import useStyles from "./landing_page_styles";
+
+export default function LandingPage() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+     
+    </div>
+  );
+}
